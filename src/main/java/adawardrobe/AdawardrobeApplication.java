12983617b66b3d0672ef6cfa932d96baa253bc33
Hellyes31 <elies.auguet@gmail.com>
2025-11-03 11:19:47 +0100

    add: setup back java/springboot with dependencies

---
+++ src/main/java/adawardrobe/AdawardrobeApplication.java
@@ -0,0 +1,13 @@
+package adawardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdawardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdawardrobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adawardrobe/AdawardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package adawardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdawardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
