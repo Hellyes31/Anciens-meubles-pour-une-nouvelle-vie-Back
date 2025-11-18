4c21983f25163673cb07bb3a58bc1fba5cef3dad
Hellyes31 <elies.auguet@gmail.com>
2025-11-18 11:25:20 +0100

    add: new error handle + authservice and controller for register an user

--- src/main/java/adawardrobe/repository/UserRepository.java
+++ src/main/java/adawardrobe/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional <User> findByUsernameOrMail(String username, String mail);
+    boolean existsByUsername(String username);
+    boolean existsByMail(String mail);
 }
